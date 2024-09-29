8d7e33b80ff2a37b0893d64ddf217ff3e6c6d640
Adam Zh. <zh.bh@qq.com>
2024-09-29 18:11:44 +1300

    fix deployment

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,6 @@
 import Content from "./content/page";
 import EventForm from "./event_form/page";
-import PopUpModal from "./modal/page";
+import PopUpModal from "./modal";
 import GoogleMap from "./map/page";
 import UserForm from "./user_form/page";
 import Auth from "./auth/page"
